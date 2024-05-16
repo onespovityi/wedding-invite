2b0503ce80253ac7ac91e47e82b962074b0d275b
Oleg Nespovityi <nespovityi_oleg@mail.ru>
2024-05-16 08:04:10 +0300

    fix contact number

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -8,7 +8,7 @@ export const Contacts = () => {
     <div ref={blockRef} className={`${cls.wrap} ${isShow ? cls.elementShow : cls.elementAnimation}`}>
       <p className={cls.title}>CONTACTS</p>
       <p className={cls.subTitle}>По всем возникшим вопросам просьба обращаться к нашему свадебному организатору</p>
-      <a href={`tel:7988586714`} className={cls.person}>Виктория</a>
+      <a href={`tel:79885867140`} className={cls.person}>Виктория</a>
       <a href='https://wa.clck.bar/79885867140' className={cls.whatsApp}>
         <img className={cls.img} src={whatsapp} width={62} height={62} alt='whatsApp' />
       </a>
